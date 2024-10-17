d5bcca66d4c7b0b25294fe7c5b0e5f73fe7200b4
Rafael Santoni <rafasantoni.dev@gmail.com>
2024-10-17 11:43:32 -0300

    Aula #06: Criado o método responsável por criar e/ou executar a conexão com o Banco de Dados e adicionada a biblioteca vlucas/phpdotenv como dependência do projeto

---
+++ App/Framework/Database/Connection.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Framework\Database;
+
+use PDO;
+
+class Connection
+{
+  private static $connection = null;
+
+  public static function getConnection()
+  {
+    if(empty(self::$connection)) {
+      self::$connection =  new PDO(
+        "mysql:host={$_ENV['DATABASE_HOST']};dbname={$_ENV['DATABASE_NAME']}", 
+        $_ENV['DATABASE_USER'], 
+        $_ENV['DATABASE_PASSWORD'], 
+        [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ]
+      );
+    }
+
+    return self::$connection;
+  }
+}
---
+++ public/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(dirname(__FILE__, 2));
+$dotenv->load();
+
+routerExecute();
